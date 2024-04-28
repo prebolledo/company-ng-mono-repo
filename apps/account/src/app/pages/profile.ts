e7ae5301ed0c303d1c2834307729db1ace6c23ae
prebolledo <34753020+prebolledo@users.noreply.github.com>
2024-04-28 15:03:15 -0400

    Update profile.ts

--- apps/account/src/app/pages/profile.ts
+++ apps/account/src/app/pages/profile.ts
@@ -1,8 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SessionStateSelector } from '@company/shared/session';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ProfileComponent } from '@company/user/profile'
-import { Store } from '@ngrx/store';
 
 @Component({
   standalone: true,
@@ -16,19 +13,4 @@ import { Store } from '@ngrx/store';
   `,
   providers:[],
 })
-export class ProfilePageComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private store: Store,
-  ){
-  }
-
-  ngOnInit(): void {
-    const token = this.store.select(SessionStateSelector.token);
-    token.subscribe((token) => {
-      if (token === null) {
-        this.router.navigate(['login']);
-      }
-    });
-  }
-}
+export class ProfilePageComponent {}
