e0af450da4bd19d8f8d09703f781b2239e086765
prebolledo <34753020+prebolledo@users.noreply.github.com>
2024-04-28 14:44:06 -0400

    Update main.ts

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-export declare const __webpack_public_path__ = 'valueFormerlyAssignedUsing_deployUrl';
-
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
